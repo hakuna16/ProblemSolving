23376f713043d32d4bfeaf6e8554d639b34fee8b
Rituj Kumar <rituj.kumar@tesco.com>
2018-06-04 18:36:26 +0530

    Adding problem 2

--- src/arrays/FindTheMissingNumber.java
+++ src/arrays/FindTheMissingNumber.java
@@ -4,6 +4,21 @@ public class FindTheMissingNumber {
 
 	public static void main(String[] args) {
 
-		int arrayInput[] = new int[] {1,2,3,4,5,6,7,8,9,10,12,13,15,16,17,18,19,20};
+		int arrayInput[] = new int[] { 1, 2, 3, 5, 6};
+
+		int arrResultBestCase = usingBestCase(arrayInput);
+		System.out.println("Using Best method..." + arrResultBestCase);
+	}
+
+	private static int usingBestCase(int[] arrayInput) {
+		
+		int sumOfNaturalNumberArray=0;
+		int n =arrayInput.length +1;
+		int sumOfNaturalNumber= (n*(n+1))/2;
+		
+		for (int i = 0; i < arrayInput.length; i++) {
+			sumOfNaturalNumberArray = arrayInput[i] + sumOfNaturalNumberArray;
+		}
+		return (sumOfNaturalNumber - sumOfNaturalNumberArray);
 	}
 }
