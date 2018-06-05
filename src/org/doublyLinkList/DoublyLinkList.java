0b6e1a8fe7af1c335ab92e47eac48fc2b183e684
Rituj Kumar <rituj.kumar@tesco.com>
2018-06-05 16:59:37 +0530

    Doubly LinkList

--- src/org/doublyLinkList/DoublyLinkList.java
+++ src/org/doublyLinkList/DoublyLinkList.java
@@ -3,16 +3,12 @@ package org.doublyLinkList;
 import org.doublyLinkList.Node;
 
 public class DoublyLinkList<T> {
-	
+
 	private Node<T> head;
 	private Node<T> tail;
-	
+
 	public Node<T> getTail() {
 		return tail;
-	}
-
-	public void setTail(Node<T> tail) {
-		this.tail = tail;
 	}
 
 	public DoublyLinkList() {
@@ -21,16 +17,34 @@ public class DoublyLinkList<T> {
 	public DoublyLinkList(final Node<T> head) {
 		this.head = head;
 	}
-	
+
 	public Node<T> getHead() {
 		return head;
 	}
 
-	public void setHead(Node<T> nodeA) {
-		this.head = nodeA;
+	public void insertFirst(final Node<T> node) {
+		if (head == null) {
+			head = node;
+			tail = node;
+			return;
+		}
+		head.setPrevious(node);
+		node.setNext(head);
+		head = node;
 	}
-	
-	public void print(){
+
+	public void insertLast(final Node<T> node) {
+		if (head == null) {
+			head = node;
+			tail = node;
+			return;
+		}
+		tail.setNext(node);
+		node.setPrevious(tail);
+		tail = node;
+	}
+
+	public void print() {
 		System.out.println();
 		Node<T> node = head;
 		while (node != null) {
@@ -38,12 +52,5 @@ public class DoublyLinkList<T> {
 			node = node.getNext();
 		}
 		System.out.print(" null");
-	}
-	
-	public void printHeadAndTail(){
-		Node<T> nodeHead = head;
-		Node<T> nodeTail = tail;
-		System.out.println();
-		System.out.println(nodeHead.getData() + "........." + nodeTail.getData());
 	}
 }
--- src/org/doublyLinkList/SettingValueInDoublyLinkList.java
+++ src/org/doublyLinkList/SettingValueInDoublyLinkList.java
@@ -6,30 +6,22 @@ public class SettingValueInDoublyLinkList {
 		
 		DoublyLinkList<String> doublyLinkList = new DoublyLinkList<>();
 		
-		Node nodeA = new Node("Er.");
-		doublyLinkList.setHead(nodeA);
-		
-		Node nodeB = new Node("Richa");
-		nodeA.setNext(nodeB);
-		
-		Node nodeC = new Node("Ritesh");
-		nodeB.setNext(nodeC);
-		
-		
-		Node nodeD = new Node("Saurabh");
-		nodeC.setNext(nodeD);
-		
-		Node nodeE = new Node("Neha");
-		nodeD.setNext(nodeE);
-		
-		Node nodeF = new Node("Vivek");
-		nodeE.setNext(nodeF);
-		
-		doublyLinkList.setTail(nodeF);
+		Node nodes[] = {new Node("Er."), new Node("Richa"), new Node("Ritesh"),  new Node("Saurabh"), new Node("Neha"), new Node("Vivek")}; 
+
+		for(Node node : nodes) {
+			doublyLinkList.insertLast(node);
+		}
 		
 		doublyLinkList.print();
 		
-		doublyLinkList.printHeadAndTail();
+		Node nodes1[] = {new Node("Er."), new Node("Richa"), new Node("Ritesh"),  new Node("Saurabh"), new Node("Neha"), new Node("Vivek")};
+		
+		doublyLinkList = new DoublyLinkList<>();
+		for(Node node : nodes1) {
+			doublyLinkList.insertFirst(node);
+		}
+		
+		doublyLinkList.print();
 	}
 
 }
